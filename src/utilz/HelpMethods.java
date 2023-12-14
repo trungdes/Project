a0af350e21d561167b0a2008237b42fbe118a69e
trungdes <ITCSIU22260@student.hcmiu.edu.vn>
2023-12-14 14:18:28 +0700

    Update HelpMethods.java

--- src/utilz/HelpMethods.java
+++ src/utilz/HelpMethods.java
@@ -4,7 +4,8 @@ import main.Game;
 
 public class HelpMethods {
 	public static boolean CanMoveHere(float x, float y, int width, int height, int[][] lvlData) {
-		 
+		 if(IsSolid(x,y,lvlData))
+			 id(!isSolid)
 		
 	}
 	public static boolean IsSolid(float x , float y,int[][] lvlData ) {
@@ -15,7 +16,8 @@ public class HelpMethods {
 		float xIndex = x / Game.TILES_SIZE;
 		float yIndex = y / Game.TILES_SIZE;
 		int value = lvlData[(int) xIndex][(int) yIndex];
-		if(value >= 48 || value < 0)
+		if(value >= 48 || value < 0 || value == 11)
+			return true;
 			
 	}
 }
